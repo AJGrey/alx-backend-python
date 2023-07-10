959899731d16fd223a59e7ecc672a3ff67191d4c
AJGrey <greyajayi@yahoo.com>
2023-07-10 23:33:37 +0100

    task fix

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env/python3
 import asyncio
+from typing import Union
 from asyncio import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
-"""returns a asyncio.Task"""
 
 
-def task_wait_random(max_delay: int) -> Task:
-    """returns a asyncio.Task"""
-    return asyncio.create_task(wait_random(max_delay))
+def task_wait_random(max_delay: int) -> Union[Task, None]:
+    """Creates and returns an asyncio.Task for wait_random"""
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(wait_random(max_delay))
+    return task
