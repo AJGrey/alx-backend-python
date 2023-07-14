67dc42e7fb683c00eaf52ccf5ec1a11b4307d6f7
AJGrey <greyajayi@yahoo.com>
2023-07-14 19:02:09 +0100

    fix

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import Union
-from asyncio import Task
+from asyncio import Task, create_task
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Union[Task, None]:
+def task_wait_random(max_delay: int) -> Task:
     """Creates and returns an asyncio.Task for wait_random"""
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(wait_random(max_delay))
+    
+    task = create_task(wait_random(max_delay))
     return task
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" Async """
+
+
 import asyncio
 from typing import List
 from heapq import nsmallest
@@ -7,7 +10,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """uses task_wait_random"""
+    """
+    uses task_wait_random n time with specified max_delay
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*tasks)
     return nsmallest(n, results)
